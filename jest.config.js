92df7fb5353323c4e9be5d6b513eca18d2d3b5aa
CyberLady <palhaxota151@gmail.com>
2026-05-24 23:44:46 -0300

    chore: migrate to Vite, add Jest+Babel test setup, CI workflow

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+};
